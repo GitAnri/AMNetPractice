f312c96d67e3618586ecdffa29f9578f97e80c5c
GitAnri <149332998+GitAnri@users.noreply.github.com>
2025-04-07 11:56:13 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,40 +4,40 @@ using NetPractice;
 
 
 //1
-//int age = 20;
-//string name = "mariami";
-//for (int i = 1; i <= 10; i++)
-//{
-//    Console.WriteLine($"{i}: {name}");
-//}
+int age = 20;
+string name = "mariami";
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{i}: {name}");
+}
 
 //2
-//Person person = new Person(15, "mariami");
-//person.Introduction();
+Person person = new Person(15, "mariami");
+person.Introduction();
 
 //3
-//Rectangle rectangle1 = new Rectangle(2, 3);
-//Circle circle1 = new Circle(3);
-//Console.WriteLine(rectangle1.CalculateArea());
-//Console.WriteLine(circle1.CalculateArea());
+Rectangle rectangle1 = new Rectangle(2, 3);
+Circle circle1 = new Circle(3);
+Console.WriteLine(rectangle1.CalculateArea());
+Console.WriteLine(circle1.CalculateArea());
 
-//Rectangle rectangle = new Rectangle();
-//Circle circle = new Circle();
-//Console.WriteLine(rectangle.CalculateArea(4, 5));
-//Console.WriteLine(circle.CalculateArea(2));
+Rectangle rectangle = new Rectangle();
+Circle circle = new Circle();
+Console.WriteLine(rectangle.CalculateArea(4, 5));
+Console.WriteLine(circle.CalculateArea(2));
 
 //ver mivxvdi obiektis shekmnistanave unda migvenichebina mnishvnelobebi tuara da orivenairad davwere
 
 //4
-//Car car = new Car();
-//Bicycle bicycle = new Bicycle();
-//car.startEngine();
-//bicycle.startEngine();
+Car car = new Car();
+Bicycle bicycle = new Bicycle();
+car.startEngine();
+bicycle.startEngine();
 
 //5
-//MyList<int> list = new MyList<int>();
-//list.Add(0);
-//list.Add(100);
-//list.Add(2);
-//Console.WriteLine(list.Get(1));
-//Console.WriteLine(list.Get(50));
+MyList<int> list = new MyList<int>();
+list.Add(0);
+list.Add(100);
+list.Add(2);
+Console.WriteLine(list.Get(1));
+Console.WriteLine(list.Get(50));
